b998a1c1c67718e0afd5210c92cf25e4bb049ae4
Juan Gabriel <juangabriel@frogames.es>
2018-09-30 11:39:15 +0200

    Instalación

---
+++ tema1/gym_install_test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import gym # cargamos la librería de OpenAI Gym
+
+environment = gym.make("SpaceInvaders-v0") # Lanzamos una instancia del videojuego de la Montaña rusa
+environment.reset() # Limpiamos y preparamos el entorno para tomar decisiones
+for _ in range(2000): # Durante 2000 iteraciones (veces)
+    environment.render() # Pintamos en pantalla la acción
+    environment.step(environment.action_space.sample()) # Tomamos una decisión aleatoria del conjunto de las disponibles 
+    
+environment.close() # Cerramos la sesión de Open AI Gym
---
+++ tema1/pytorch_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 30 11:33:11 2018
+
+@author: juangabriel
+"""
+
+import torch
+
+t = torch.Tensor(3,3)
+print(t)
